1e60ac44ee690f0b3bc245daf9c9cedbbbd54444
MOBSkuchen <mobskuchen@gmail.com>
2026-07-24 12:44:48 +0200

    persistent comms

--- bierpc/src/error.rs
+++ bierpc/src/error.rs
@@ -2,16 +2,21 @@ use std::io;
 use crate::serialize::{Serialize, Deserialize};
 use bier_derive::{Deserialize, Serialize};
 
-// Dead wrapper for an IO Error. Might be expanded in the future
 #[derive(Serialize, Deserialize)]
 #[derive(Debug)]
 pub enum RpcError {
-    IoError,
+    IoError {
+        err: io::Error
+    },
+    CallTypeRejected {
+        reason: String
+    },
+    ConnectionPoisoned
 }
 
 impl From<io::Error> for RpcError {
-    fn from(_e: io::Error) -> Self {
-        RpcError::IoError
+    fn from(err: io::Error) -> Self {
+        RpcError::IoError { err }
     }
 }
 
